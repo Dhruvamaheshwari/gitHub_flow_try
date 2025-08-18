cc624cc7b8c8e5c8fc84201cbc6ee99fa5c9ecf8
SAYOUNCDR <pheonixgamer12345@gmail.com>
2025-08-18 12:30:55 +0530

    Second smallest

--- array-dsa-roadmap/01_Basic_OP/Second_Largest.cpp
+++ array-dsa-roadmap/01_Basic_OP/Second_Largest.cpp
@@ -1,12 +1,32 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
+
+int secondSmallest(vector<int> &arr)
+{
+    if (arr.size() < 2)
+        return -1;
+
+    sort(arr.begin(), arr.end());
+
+    int smallest = arr[0];
+    for (int i = 1; i < arr.size(); i++)
+    {
+        if (arr[i] > smallest)
+        {
+            return arr[i];
+        }
+    }
+    return -1;
+}
 
 int main()
 {
     int size;
     cin >> size;
-    if(size <= 0){
+    if (size <= 0)
+    {
         cout << "The array is empty";
         return 0;
     }
@@ -17,8 +37,15 @@ int main()
         cin >> arr[i];
     }
 
-    
-
+    int result = secondSmallest(arr);
+    if (result == -1)
+    {
+        cout << "All elements in array is same";
+    }
+    else
+    {
+        cout << "Second smallest element in the array is : " << result;
+    }
 
     return 0;
 }
